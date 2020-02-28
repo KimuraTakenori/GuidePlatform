8e406089574c5cd1c5f191ff11724d9a72919acf
木村剛徳 <kimura@pc21107.bioinfo.i.ttck.keio.ac.jp>
2020-02-28 18:37:48 +0900

    202002281837

--- appGuide/admin.py
+++ appGuide/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tourist, Guide, Gender
+from .models import Gender, Tourist, Guide, Preplace, Place, Reservation,Guidetime, Guideplace
 
+admin.site.register(Gender)
 admin.site.register(Tourist)
 admin.site.register(Guide)
-admin.site.register(Gender)
+admin.site.register(Preplace)
+admin.site.register(Place)
+admin.site.register(Reservation)
+admin.site.register(Guidetime)
+admin.site.register(Guideplace)
--- appGuide/models.py
+++ appGuide/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+#性別データ
 class Gender(models.Model):
 
     gender_name = models.CharField(max_length = 32, null = True, blank = True,
@@ -11,6 +12,7 @@ class Gender(models.Model):
 
         return self.gender_name
 
+#利用者データ
 class Tourist(models.Model):
 
     user_sei = models.CharField(max_length = 32, null = True, blank = True,
@@ -45,7 +47,7 @@ class Tourist(models.Model):
 
         return self.user_sei + " " + self.user_mei
 
-
+#ガイドデータ
 class Guide(models.Model):
 
     guide_sei = models.CharField(max_length = 32, null = True, blank = True,
@@ -80,7 +82,132 @@ class Guide(models.Model):
     guide_pr = models.CharField(max_length = 1000, null = True, blank = True,
                                 verbose_name = "PR欄")
 
-
     def __str__(self):
 
         return self.guide_sei + " " + self.guide_mei
+
+#プレ観光地データ
+class Preplace(models.Model):
+
+    preplace_address1 = models.CharField(max_length = 10, null = True, blank = True,
+                                     verbose_name = "都道府県")
+    preplace_address2 = models.CharField(max_length = 32, null = True, blank = True,
+                                     verbose_name = "市区町村")
+    preplace_name = models.CharField(max_length = 64, null = True, blank = True,
+                                     verbose_name = "観光地名称")
+    preplace_info = models.CharField(max_length = 500, null = True, blank = True,
+                                     verbose_name = "観光地情報")
+
+    def __str__(self):
+
+        return self.preplace_address1 + " " + self.preplace_address2 + " " + self.preplace_name
+
+#観光地データ
+class Place(models.Model):
+
+    place_address1 = models.CharField(max_length = 10, null = True, blank = True,
+                                     verbose_name = "都道府県")
+    place_address2 = models.CharField(max_length = 32, null = True, blank = True,
+                                     verbose_name = "市区町村")
+    place_name = models.CharField(max_length = 64, null = True, blank = True,
+                                     verbose_name = "観光地名称")
+    place_info = models.CharField(max_length = 500, null = True, blank = True,
+                                     verbose_name = "観光地情報")
+
+    def __str__(self):
+
+        return self.place_address1 + " " + self.place_address2 + " " + self.place_name
+
+#手配データ
+class Reservation(models.Model):
+
+    reservation_name = models.ForeignKey(Tourist,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True,
+                                    verbose_name = "利用者名")
+    reservation_ymdt = models.DateTimeField(null = True, blank = True, verbose_name = "予約日時")
+    reservation_place = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True,
+                                    verbose_name = "観光地")
+    reservation_guide = models.ForeignKey(Guide,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True,
+                                    verbose_name = "ガイド")
+
+#ガイド可能時間データ
+class Guidetime(models.Model):
+
+    guide_name = models.ForeignKey(Guide,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True,
+                                    verbose_name = "ガイド名")
+    guide_time1 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間1")
+    guide_time2 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間2")
+    guide_time3 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間3")
+    guide_time4 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間4")
+    guide_time5 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間5")
+    guide_time6 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間6")
+    guide_time7 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間7")
+    guide_time8 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間8")
+    guide_time9 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間9")
+    guide_time10 = models.DateTimeField(null = True, blank = True, verbose_name = "ガイド可能時間10")
+
+#ガイド可能場所データ
+class Guideplace(models.Model):
+
+    guide_name = models.ForeignKey(Guide,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True,
+                                    verbose_name = "ガイド名")
+    guide_place1 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place1",
+                                    verbose_name = "ガイド可能場所1")
+    guide_place2 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place2",
+                                    verbose_name = "ガイド可能場所2")
+    guide_place3 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place3",
+                                    verbose_name = "ガイド可能場所3")
+    guide_place4 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place4",
+                                    verbose_name = "ガイド可能場所4")
+    guide_place5 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place5",
+                                    verbose_name = "ガイド可能場所5")
+    guide_place6 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place6",
+                                    verbose_name = "ガイド可能場所6")
+    guide_place7 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place7",
+                                    verbose_name = "ガイド可能場所7")
+    guide_place8 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place8",
+                                    verbose_name = "ガイド可能場所8")
+    guide_place9 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place19",
+                                    verbose_name = "ガイド可能場所9")
+    guide_place10 = models.ForeignKey(Place,
+                                    on_delete=models.CASCADE,
+                                    null = True, blank = True, related_name = "guide_place10",
+                                    verbose_name = "ガイド可能場所10")
+
+
+
+
+
+
+
+
+
+
+
